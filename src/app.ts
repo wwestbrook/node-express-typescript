2dc92c05d74530ea3d85b74dc7209012d0c7a5a0
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2021-02-20 00:50:41 -0500

    ww

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,2 @@
-console.log('working app');
-
-const msg = 'node, ts, nodemon up and running';
-console.log(msg);
-
-class Dog {
-	constructor(public name: string) {
-		this.name = name;
-	}
-}
-
-const leo = new Dog('Leonidas');
-console.log(leo);
+// import * as fs from 'fs';
+// fs.writeFileSync('_hello.txt', 'hello from node.js!');
