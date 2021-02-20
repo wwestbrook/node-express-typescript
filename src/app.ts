ae595e8c038f787becdc8426193bd303ca2cc5c7
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2021-02-20 15:45:54 -0500

    ts module imports working for node

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // import * as fs from 'fs';
 // fs.writeFileSync('_hello.txt', 'hello from node.js!');
+
+import * as http from 'http';
+import requestHandler from './routes';
+
+console.log(requestHandler);
+const server = http.createServer(requestHandler);
+
+server.listen(3000);
---
+++ src/routes.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as http from 'http';
+
+// eslint-disable-next-line max-len
+const requestHandler = (req: http.IncomingMessage, res: http.ServerResponse): void | http.IncomingMessage => {
+	const { url, method } = req;
+	if (url === '/') {
+		res.write('<html>');
+		res.write('<head><title>Input Page</title></head>');
+		res.write(
+			'<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>'
+		);
+		res.write('</html>');
+		return res.end();
+	}
+
+	const body: Uint8Array[] = [];
+	if (url === '/message' && method === 'POST') {
+		req.on('data', (chunk) => {
+			// console.log(chunk);
+			body.push(chunk);
+		});
+		return req.on('end', () => {
+			const parsedBody = Buffer.concat(body).toString();
+			// console.log(parsedBody);
+			fs.writeFile('_message.txt', parsedBody.split('=')[1], (error) => {
+				if (error) {
+					console.log(error);
+					return;
+				}
+
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+				res.end();
+			});
+		});
+	}
+
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title>First Node Page</title></head>');
+	res.write('<body><h1>Hello from my node app!!</h1></body>');
+	res.write('</html>');
+	return res.end();
+};
+
+export default requestHandler;
