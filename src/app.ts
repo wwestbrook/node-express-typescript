5e2d66c3ab21ba9fed4e2f3c23b8a5c59369b72c
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2021-02-26 15:28:42 -0500

    cleaning up routing, merging routes to a single file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-// import adminRouter from './routes/admin';
-// import itemsRouter from './routes/items';
 import router from './routes';
 import * as errorController from './controllers/error-controller';
 
@@ -15,8 +13,6 @@ app.set('views', 'src/views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// app.use('/admin', adminRouter);
-// app.use(itemsRouter);
 app.use(router);
 
 app.use(errorController.get404Error);
